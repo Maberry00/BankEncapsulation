6266d0205d7205e552c74e91da834d1a46d4f43a
Maberry00 <maberry30@gmail.com>
2024-01-25 11:27:33 -0600

    bank

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount();
+
+            Console.WriteLine($"How much money do you want to deposit?");
+            double amountToDeposit = double.Parse( Console.ReadLine());
+
+            account.Deposit( amountToDeposit );
+            double userBalance = account.GetBalance();
+
+            Console.WriteLine($"Your current balance is : {userBalance,0:c}");
+
+           
+
         }
     }
 }
